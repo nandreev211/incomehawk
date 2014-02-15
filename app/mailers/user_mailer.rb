5386c895843a7b2798df633e55a8ec9228365cb1
Nikolay Andreev <n.andreev.211@gmail.com>
2014-02-15 12:42:50 +0200

    Incomehawk live

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
   # send welcome email to customer on their registration
   def welcome_email(user)
     @user = user
-    @url  = 'http://incomehawkapp.com/login'
+    @url  = 'http://incomehawkapp-com.herokuapp.comlogin'
     mail(to: @user.email, subject: 'Welcome to My Site')
   end
 end
